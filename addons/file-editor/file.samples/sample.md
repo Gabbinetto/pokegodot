cb7deb5cfe977cf95123a71da4a57e5ddad949e5
Gabbinetto <gabrieleferrante97@googlemail.com>
2022-08-19 15:52:32 +0200

    First commit

---
+++ addons/file-editor/file.samples/sample.md
@@ -0,0 +1,6 @@
+**bolded text**
+*italicized text*
+~~strikethrough~~
+__simple__
+[link](http://example.com)
+![image](res://addons/file-editor/icon.png)
---
+++ addons/file-editor/scripts/file-editor.gd
@@ -0,0 +1,32 @@
+tool
+extends EditorPlugin
+
+var doc = preload("../scenes/FileEditor.tscn").instance()
+var plugin_version
+var toolbar
+var fileditorbutton
+var IconLoader = preload("res://addons/file-editor/scripts/IconLoader.gd")
+
+func _enter_tree():
+	add_autoload_singleton("IconLoader","res://addons/file-editor/scripts/IconLoader.gd")
+	add_autoload_singleton("LastOpenedFiles","res://addons/file-editor/scripts/LastOpenedFiles.gd")
+	get_editor_interface().get_editor_viewport().add_child(doc)
+	doc.hide()
+
+func _exit_tree():
+	doc.clean_editor()
+	get_editor_interface().get_editor_viewport().remove_child(doc)
+	remove_autoload_singleton("IconLoader")
+	remove_autoload_singleton("LastOpenedFiles")
+
+func has_main_screen():
+	return true
+
+func get_plugin_name():
+	return "File"
+
+func get_plugin_icon():
+	return IconLoader.load_icon_from_name("file")
+
+func make_visible(visible):
+	doc.visible = visible
